75db04994b3c48a5052c29abb764a5a0e621f0bd
Sadaf Farooq <xadaffarooq@gmail.com>
2026-07-10 12:45:57 +0500

    Added a search input to search products with useState

--- UseMemo Tasks/src/components/filtered list.jsx
+++ UseMemo Tasks/src/components/filtered list.jsx
@@ -1,15 +1,26 @@
 import { products } from "../data/products";
+import { useState } from "react";
 
 function FileteredList(){
-    // const [category, setCategory] = useState("all");
-    // const [searchItem, setSearchItem] = useState("");
+    const [category, setCategory] = useState("all");
+    const [searchItem, setSearchItem] = useState("");
+
+function handleProductsResult(e){
+    setSearchItem(e.target.value);
+    setCategory(e.target.value);
+}
+let FileteredList= products.filter((product)=>(
+  product.name.toLowerCase().includes(searchItem.toLowerCase())  
+))
 
     return(
         <>
 <h3>Products List</h3>
 
+<input type=" text" placeholder="Search item.." value={searchItem} onChange={handleProductsResult}/>
+
         <ul>
-            {products.map((product) => ( 
+            {FileteredList.map((product) => ( 
                 <li key={product.id}>{product.name} - rs {product.price}</li>
             ))}
             </ul>

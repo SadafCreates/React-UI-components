ee28ee1dd9b115677151990f0bfc6c6ea15adb25
Sadaf Farooq <xadaffarooq@gmail.com>
2026-07-10 17:13:41 +0500

    Added useMemo logic to filter prodcts to avoid unnecessary rendering

--- UseMemo Tasks/src/components/filtered list.jsx
+++ UseMemo Tasks/src/components/filtered list.jsx
@@ -1,9 +1,13 @@
 import { products } from "../data/products";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 function FileteredList(){
-    const [category, setCategory] = useState("all");
+    const [category, setCategory] = useState("All");
     const [searchItem, setSearchItem] = useState("");
+    const [count, setCount] = useState(0);
+
+    console.log('rendered');
+
 
 function SearchHandler(e){
     setSearchItem(e.target.value);
@@ -13,11 +17,13 @@ function categoryHandler(e){
         setCategory(e.target.value);
 }
 
-let FilteredList= products.filter((product)=>{
+let FilteredList= useMemo(()=>{
+    console.log('Filtered objects');
+    return products.filter((product)=>{
     const searchMatch= product.name.toLowerCase().includes(searchItem.toLowerCase())  
     const categoryMatch = category === 'All' || product.category===category
 return searchMatch && categoryMatch;
-});
+})},[searchItem , category])
     return(
         <>
 <h3>Products List</h3>
@@ -34,6 +40,8 @@ return searchMatch && categoryMatch;
                 <li key={product.id}>{product.name} - rs {product.price}</li>
             ))}
             </ul>
+            <button onClick={()=>setCount(count+1)}>{count}</button>
+
             </>
     )
 }
